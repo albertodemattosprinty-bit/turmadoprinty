eecea03f0cb0ebc1a62dc614e68bf27539feb7ce
Alberto de Mattos <albertodemattos.printy@gmail.com>
2026-08-05 07:50:20 -0300

    Version Project 200 APK update to 0.81

--- public/201/app.js
+++ public/201/app.js
@@ -1,7 +1,7 @@
-﻿import { getApiUrl } from "../api.js";
+import { getApiUrl } from "../api.js";
 
 const tokenKey = "turma_do_printy_token";
-const releaseVersion = "0.80";
+const releaseVersion = "0.81";
 const defaults = {
   currentVersion: releaseVersion,
   minimumVersion: "0.71",
