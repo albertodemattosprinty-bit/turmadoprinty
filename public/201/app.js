424aa55aab62c7890017070e4bfb723e4937bdef
Alberto de Mattos <albertodemattos.printy@gmail.com>
2026-08-31 08:43:17 -0300

    feat(project200): style apps home with apple tiles

--- public/201/app.js
+++ public/201/app.js
@@ -1,7 +1,7 @@
 import { getApiUrl } from "../api.js";
 
 const tokenKey = "turma_do_printy_token";
-const releaseVersion = "1.04";
+const releaseVersion = "1.05";
 const defaults = {
   currentVersion: releaseVersion,
   minimumVersion: "0.71",
